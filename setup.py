570dd469404fca768c5e6a194c8ff9181c5df648
PowerfulBean <25721564+powerfulbean@users.noreply.github.com>
2025-02-12 02:42:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
   name="nntrf",
-  version="1.0.1",
+  version="1.0.2",
   author="Jin Dou",
   author_email="jindou.bci@gmail.com",
   long_description_content_type="text/markdown",
